1a2b2c30108b8e837ad9bc4aca4baff30ee11a90
bobdolectf <46633387+bobdolectf@users.noreply.github.com>
2024-01-24 17:01:25 -0500

    Update variables.tf

--- ad/GOAD/providers/gcp/terraform/variables.tf
+++ ad/GOAD/providers/gcp/terraform/variables.tf
@@ -8,10 +8,6 @@ variable "size" {
   default = "e2-standard-2"  // Using a GCP machine type
 }
 
-resource "google_folder" "resource_group" {
-  display_name = "GOAD"
-  parent       = "organizations/366616858145"  // Replace with your organization ID
-}
 
 resource "google_compute_network" "vpc" {
   name                    = "goad-virtual-network"
