6d679764d99b4a1220052cc16cb4fb94cb2fbe15
bobdolectf <46633387+bobdolectf@users.noreply.github.com>
2024-01-24 17:09:53 -0500

    Update variables.tf

--- ad/GOAD/providers/gcp/terraform/variables.tf
+++ ad/GOAD/providers/gcp/terraform/variables.tf
@@ -17,7 +17,7 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "goad-subnet" {
-  name          = "goat-vm-subnet"
+  name          = "goad-vm-subnet"
   ip_cidr_range = "192.168.56.0/24"
   region        = var.location  // Set region directly in the subnetwork
   network       = google_compute_network.vpc.self_link
