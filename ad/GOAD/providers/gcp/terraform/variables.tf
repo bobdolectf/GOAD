28cd3638608ab55312b2691b90220ee12b208616
bobdolectf <46633387+bobdolectf@users.noreply.github.com>
2024-01-24 16:53:39 -0500

    Update variables.tf

--- ad/GOAD/providers/gcp/terraform/variables.tf
+++ ad/GOAD/providers/gcp/terraform/variables.tf
@@ -13,14 +13,14 @@ resource "google_folder" "resource_group" {
   parent       = "organizations/366616858145"  // Replace with your organization ID
 }
 
-resource "google_compute_network" "virtual_network" {
+resource "google_compute_network" "vpc" {
   name                    = "goad-virtual-network"
   auto_create_subnetworks = false  // Disable automatic subnetwork creation
   routing_mode            = "REGIONAL"  // Set routing mode to REGIONAL
   mtu                     = 1460
 }
 
-resource "google_compute_subnetwork" "subnet" {
+resource "google_compute_subnetwork" "goad-subnet" {
   name          = "goat-vm-subnet"
   ip_cidr_range = "192.168.56.0/24"
   region        = var.location  // Set region directly in the subnetwork
